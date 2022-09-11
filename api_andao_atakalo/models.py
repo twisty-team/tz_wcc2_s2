77d80a5df2fc113a6b0a9c0c90d7bd6b180c8ec1
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-09-11 12:25:09 +0300

    Update Models

--- api_andao_atakalo/models.py
+++ api_andao_atakalo/models.py
@@ -13,6 +13,7 @@ class Owner(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True, null=False, blank=False)
     contact = PhoneNumberField(unique=True, null=False, blank=False)
+    token = models.CharField(max_length=255, default=generate_token, unique=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +24,6 @@ class Exchange(models.Model):
     toy_to_change = models.CharField(max_length=255)
     desired_toy = models.CharField(max_length=255)
     owner = models.ForeignKey(Owner, related_name="toys",on_delete=models.CASCADE)
-    token = models.CharField(max_length=255, default=generate_token, unique=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
